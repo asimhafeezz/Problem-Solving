57ed1aeb754d6e488d0f573a52685ae4f8dd904a
asimhafeezz <asimhaff.social@gmail.com>
2022-11-05 12:41:57 +0500

    refactor bubble sort

--- bubble sort.js
+++ bubble sort.js
@@ -5,9 +5,7 @@ function sort(arr) {
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = i + 1; j < arr.length; j++) {
 			if (arr[i] > arr[j]) {
-				let temp = arr[i]
-				arr[i] = arr[j]
-				arr[j] = temp
+				;[arr[i], arr[j]] = [arr[j], arr[i]]
 			}
 		}
 	}
