bea3b42664f16d8f87c466712ac0c4300b6c6e22
asimhafeezz <asimhaff.social@gmail.com>
2022-11-06 16:17:59 +0500

    three number sum

---
+++ Three Number Sum.js
@@ -0,0 +1,30 @@
+const arr = [12, 3, 1, 2, -6, 5, -8, 6]
+const targetSum = 0
+
+const threeNumberSum = (array, targetSum) => {
+	array.sort((a, b) => a - b)
+
+	let arr = []
+
+	for (let i = 0; i < array.length; i++) {
+		let num = array[i]
+		let leftPtr = i + 1
+		let rightPtr = array.length - 1
+		while (leftPtr < rightPtr) {
+			const result = num + array[leftPtr] + array[rightPtr]
+			console.log({ result })
+			if (result === targetSum) {
+				arr.push([num, array[leftPtr], array[rightPtr]])
+				leftPtr++
+				rightPtr--
+			} else if (result < targetSum) {
+				leftPtr++
+			} else if (result > targetSum) {
+				rightPtr--
+			}
+		}
+	}
+	return arr
+}
+
+console.log(threeNumberSum(arr, targetSum))
